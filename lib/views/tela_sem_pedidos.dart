4784d80c163e2bdb1271c87635d7b89ceb89c26f
Lucas Henryck <lucashenryck071@gmail.com>
2024-07-17 00:11:11 -0400

    Finalização e entrega do aplicativo

---
+++ lib/views/tela_sem_pedidos.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TelaSemPedidos extends StatelessWidget {
+  final Future<void> Function() onRefresh;
+
+  const TelaSemPedidos({super.key, required this.onRefresh});
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/deliveryman.png',
+                width: 500,
+              ),
+              Text(
+                "Entregas finalizadas.",
+                style: GoogleFonts.dmSans(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "Bom trabalho!",
+                style: GoogleFonts.dmSans(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
